d62fa63380df6927df113748c77f5add3994cea0
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-03-30 21:06:09 +0700

    #14 Configure Permission Based Authorization

--- src/main/java/rc/bootsecurity/SecurityConfiguration.java
+++ src/main/java/rc/bootsecurity/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,11 +17,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2")
                 .and()
-                .withUser("john").password(passwordEncoder().encode("john123")).roles("USER")
+                .withUser("john")
+                .password(passwordEncoder().encode("john123"))
+                .roles("USER")
                 .and()
-                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGEMENT");
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGEMENT").authorities("ACCESS_TEST1");
     }
 
     @Override
@@ -32,15 +37,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/index.html").permitAll()
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/management/**").hasAnyRole("ADMIN","MANAGEMENT")
-                .antMatchers("/api/public/**").authenticated()
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGEMENT")
+                .antMatchers("/api/public/test1").hasAnyAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAnyAuthority("ACCESS_TEST2")
                 .and()
                 .httpBasic();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder()
-    {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
